9217c9201a72359c165ccc4c9f3dee2295a0073a
Shazvi <shazvi@outlook.com>
2019-03-19 07:18:07 +0530

    webhook test

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 // test string
 // test string
 // test string
+// test string
 if($_SERVER["HTTP_HOST"] != "localhost") {
     if( !isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) && ($_SERVER["HTTP_X_FORWARDED_PROTO"] != "https") ) {
         header('Location: https://shazvi.com');
